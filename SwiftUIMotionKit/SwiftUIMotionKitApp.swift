bded5584af7ba09130300025fddcb651c2db2b80
Denidu Gamage <denidugamage21@gmail.com>
2025-05-20 16:32:18 +0530

    Initial Commit

---
+++ SwiftUIMotionKit/SwiftUIMotionKitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMotionKitApp.swift
+//  SwiftUIMotionKit
+//
+//  Created by Denidu Gamage on 2025-05-20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIMotionKitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUIMotionKitTests/SwiftUIMotionKitTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIMotionKitTests.swift
+//  SwiftUIMotionKitTests
+//
+//  Created by Denidu Gamage on 2025-05-20.
+//
+
+import Testing
+@testable import SwiftUIMotionKit
+
+struct SwiftUIMotionKitTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
